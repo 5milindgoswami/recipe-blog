18783229df719955df4969e78e975dc3abeee8b7
5milindgoswami <milindgoswami5@gmail.com>
2024-07-25 14:59:04 +0530

    new

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require("connect-flash");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 require("dotenv").config();
 
@@ -28,7 +28,7 @@ app.use(fileUpload());
 app.set("layout", "./layouts/main");
 app.set('view engine', "ejs")
 
-const routes = require("./routes/recipeRoutes.js");
+const routes = require("./server/routes/recipeRoutes.js");
 app.use("/", routes);
 
 app.listen(port, ()=>{
